43848138cbc4174b9eb08248a96bebc76d655e6e
hanieh <rahimihaniyeh58@gmail.com>
2026-09-19 11:57:25 +0330

    dd train-test split, KNN pipeline, and baseline evaluation

--- knn_model.py
+++ knn_model.py
@@ -1,5 +1,10 @@
 import pandas as pd
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 
 #Load Dataset
 data = load_breast_cancer()
@@ -15,3 +20,24 @@ y = pd.Series(data.target,name="target")
 # print(X.isnull().sum())
 
 
+#Train / Test Split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+
+#Create KNN Pipeline
+knn_pipeline = Pipeline([
+    ("scaler",StandardScaler()),
+    ("classifier",KNeighborsClassifier())
+])
+
+#Initial KNN Model
+knn_pipeline.fit(X_train,y_train)
+
+y_pred =knn_pipeline.predict(X_test)
+before_accuracy = accuracy_score(y_test,y_pred)
+
+print("Test Accuracy: ",before_accuracy)
+
+print("Confusion matrix: ",confusion_matrix(y_test,y_pred))
+
+print("Classification Report: ")
+print(classification_report(y_test,y_pred))
